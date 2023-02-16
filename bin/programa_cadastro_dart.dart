1728d01c7d9d348e79a674fe6078b86471e2e11f
Bruno Mota <bmotadev@gmail.com>
2023-02-15 21:12:52 -0300

    projeto inicializado

---
+++ bin/programa_cadastro_dart.dart
@@ -0,0 +1,5 @@
+import 'package:programa_cadastro_dart/programa_cadastro_dart.dart' as programa_cadastro_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${programa_cadastro_dart.calculate()}!');
+}
---
+++ test/programa_cadastro_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:programa_cadastro_dart/programa_cadastro_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
